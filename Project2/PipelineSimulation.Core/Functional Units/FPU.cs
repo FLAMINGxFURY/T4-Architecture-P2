7a56d05553bc8edada769f28c791828ce9fb0c67
Destin Hebner <hebnerd@etsu.edu>
2021-11-08 15:33:32 -0500

    added some FPU behavior

--- Project2/PipelineSimulation.Core/Functional Units/FPU.cs
+++ Project2/PipelineSimulation.Core/Functional Units/FPU.cs
@@ -1,28 +1,82 @@
-﻿using PipelineSimulation.Core.Instructions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace PipelineSimulation.Core.Functional_Units {
+namespace PipelineSimulation.Core.Functional_Units 
+{
 	public class FPU : FunctionalUnit
 	{
-		//TODO: Registers and secial FPU functs
+		/// <summary>
+		/// Refers to the top of the FPUStack. Is actually three bits in reality.
+		/// </summary>
+		public byte TOP { get; set; } = MAX_REG - 1;
+		private Half?[] _FPUStack { get; set; } = new Half?[MAX_REG];
+		private const int MAX_REG = 8;
 
-
-		public FPU()
+		/// <summary>
+		/// Pushes a value onto the FPU stack. If there's data that would be 
+		/// overwritten, an exception will be thrown.
+		/// </summary>
+		/// <param name="data">The data to push.</param>
+		public void Push(Half data)
 		{
-			foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+			TOP--;
+			if (TOP < 0)
 			{
-				if (type.BaseType == typeof(Instruction))
-				{
-					var instruction = (Instruction)Activator.CreateInstance(type, this);
-					if (instruction.OpCode > 0x0F && instruction.OpCode < 0x1E) //this should include ALL floating point instructions
-						_operations.Add(instruction.OpCode, instruction);
-				}
+				TOP = MAX_REG - 1;
 			}
+
+			if (_FPUStack[TOP].HasValue)
+            {
+				throw new Exception("Floating point exception. Attemped to overwite values on the FPU stack.");
+            }
+
+			_FPUStack[TOP] = data;		
+        }
+
+		/// <summary>
+		/// Pops a value from the FPU stack. If there's no data to pop, an 
+		/// exception will be thrown.
+		/// </summary>
+		public Half Pop()
+        {
+			if (!_FPUStack[TOP].HasValue)
+            {
+				throw new Exception("Floating point exception. Attempted to pop value that does not exist.");
+			}
+
+			var data = _FPUStack[TOP];
+			_FPUStack[TOP] = null;
+
+			TOP++;
+			if (TOP > MAX_REG - 1)
+			{
+				TOP = 0;
+			}
+
+			return data.Value;
 		}
+
+		/// <summary>
+		/// Reads the value on the stack at the position specified. Will return 
+		/// null if no value is present. Index refers to the offset from TOP.
+		/// For example, ST(0) (index = 0), means that it gets the value at TOP.
+		/// ST(1) means that it gets TOP  + 1.
+		/// </summary>
+		public Half? Read(int index)
+        {
+			index = TOP + index;
+			if (index > MAX_REG - 1)
+            {
+				index -= MAX_REG;
+            }
+
+			if (_FPUStack[index].HasValue)
+            {
+				return _FPUStack[index].Value;
+            }
+			else
+            {
+				return null;
+            }
+        }
 	}
 }

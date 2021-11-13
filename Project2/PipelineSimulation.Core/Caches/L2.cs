f2ab5f73a4eab6b261a2d8edcd04da3fd108f9e7
FLAMINGxFURY <fury.etain.5@gmail.com>
2021-11-13 18:52:43 -0500

    Cache Skeleton

---
+++ Project2/PipelineSimulation.Core/Caches/L2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineSimulation.Core.Caches {
+	class L2 : Cache {
+	}
+}
